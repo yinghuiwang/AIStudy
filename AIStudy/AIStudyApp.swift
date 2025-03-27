db3d25f72b8d978b0ad24a919da4c93f98efb44b
mark <wangyinghui@sparkla.cn>
2025-03-27 20:55:22 +0800

    Initial Commit

---
+++ AIStudy/AIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIStudyApp.swift
+//  AIStudy
+//
+//  Created by mark on 2025/3/27.
+//
+
+import SwiftUI
+
+@main
+struct AIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
